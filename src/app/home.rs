06bb3fb22a212b176b93559583989f023d89bff1
Ryan <ryanrothweiler@gmail.com>
2024-04-21 20:00:17 -0700

    more account layout

--- src/app/home.rs
+++ src/app/home.rs
@@ -104,19 +104,33 @@ pub fn Home() -> impl IntoView {
     };
 
     view! {
-         <h1>
-             "Home"
-         </h1>
 
-         <h3>
-             "Accounts"
-         </h3>
+        <div class="container-fluid">
+          <div class="row">
 
-         <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#account_create">
-             "Add Account"
-         </button>
+            <div class="col">
+                <h2 class="text-secondary">
+                    "Accounts"
+                </h2>
+            </div>
+            <div class="col text-end">
+                <button type="button" class="btn btn-secondary" data-bs-toggle="modal" data-bs-target="#account_create">
 
-         <div class="modal fade" id="account_create" tabindex="-1" aria-labelledby="account_create" aria-hidden="true">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16">
+                      <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
+                      <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4"/>
+                    </svg>
+                    "Add Account"
+                </button>
+
+
+
+            </div>
+
+          </div>
+        </div>
+
+                <div class="modal fade" id="account_create" tabindex="-1" aria-labelledby="account_create" aria-hidden="true">
            <div class="modal-dialog modal-dialog-centered">
              <div class="modal-content">
                <div class="modal-header">
