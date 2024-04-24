f1ca75b52ef843bbf916efc1fda5bc9d06bd9faa
Ryan <ryanrothweiler@gmail.com>
2024-04-24 15:36:11 -0700

    leptos_charts working sample project

--- src/app/home.rs
+++ src/app/home.rs
@@ -3,6 +3,8 @@ use leptos::leptos_dom::ev::{MouseEvent, SubmitEvent};
 use leptos::logging::*;
 use leptos::*;
 
+use leptos_chartistry::*;
+
 use wasm_bindgen::prelude::*;
 
 use serde::{Deserialize, Serialize};
@@ -16,6 +18,12 @@ async fn get_account_list() -> Vec<AccountDisplay> {
     let ret_js: JsValue = super::invoke("get_account_display_list", JsValue::NULL).await;
     let ret: ResultWrapped<Vec<AccountDisplay>, String> = from_value(ret_js).unwrap();
     return ret.res.unwrap();
+}
+
+struct MyData {
+    x: f64,
+    y1: f64,
+    y2: f64,
 }
 
 #[component]
@@ -103,9 +111,48 @@ pub fn Home() -> impl IntoView {
         });
     };
 
-    view! {
+    let (data_get, data_set) = create_signal(vec![
+        MyData {
+            x: 0.0,
+            y1: 0.0,
+            y2: 0.0,
+        },
+        MyData {
+            x: 10.0,
+            y1: 20.0,
+            y2: 15.0,
+        },
+    ]);
 
+    view! {
         <div class="container-fluid">
+
+    <Chart
+        // Sets the width and height
+        aspect_ratio=AspectRatio::from_outer_ratio(600.0, 300.0)
+
+        // Decorate our chart
+        top = RotatedLabel::middle("My garden")
+        left = TickLabels::aligned_floats()
+        right = leptos_chartistry::Legend::end()
+        bottom = TickLabels::aligned_floats()
+        inner = [
+            AxisMarker::left_edge().into_inner(),
+            AxisMarker::bottom_edge().into_inner(),
+            XGridLine::default().into_inner(),
+            YGridLine::default().into_inner(),
+            XGuideLine::over_data().into_inner(),
+            YGuideLine::over_mouse().into_inner(),
+        ]
+        tooltip = Tooltip::left_cursor()
+
+        // Describe the data
+        series = Series::new(|data: &MyData| data.x)
+            .line(Line::new(|data: &MyData| data.y1).with_name("butterflies"))
+        data = data_get
+    />
+
+
           <div class="row">
 
             <div class="col">
